d89976e622a8e1c6b1f9527209e9a71f3844c579
giuligelardi <ggelardi98@gmail.com>
2022-07-18 23:24:47 -0300

    events

--- miarchivo.js
+++ miarchivo.js
@@ -1,127 +1,104 @@
 class Mate {
-  constructor(nombre, precio, id) {
-    this.nombre = nombre.toLocaleLowerCase();
+  constructor(nombre, precio, id, imagen, descripcion) {
+    this.nombre = nombre;
     this.precio = parseFloat(precio);
     this.id = id;
+    this.imagen = imagen;
+    this.descripcion = descripcion;
   }
 }
 
-let mates = []
+let mates = [];
 
-const mateImp = new Mate("Mate Imperial", 5000, "AN1");
-const mateTor = new Mate("Mate Torpedo", 3000, "AN2");
-const mateCam = new Mate("Mate Camionero", 3500, "AN3");
-const bombAlp = new Mate("Bombilla de Alpaca", 2200, "AN4");
-const bombAce = new Mate("Bombilla de Acero", 1200, "AN5");
+const mateImp = new Mate(
+  "Mate Imperial",
+  5000,
+  "AN1",
+  "./media/img/Card1.jpg",
+  "descripcion"
+);
+const mateTor = new Mate(
+  "Mate Torpedo",
+  3000,
+  "AN2",
+  "./media/img/Card2.jpg",
+  "descripcion"
+);
+const mateCam = new Mate(
+  "Mate Camionero",
+  3500,
+  "AN3",
+  "./media/img/Card3.jpg",
+  "descripcion"
+);
+const bombAlp = new Mate(
+  "Bombilla de Alpaca",
+  2200,
+  "AN4",
+  "./media/img/P1.jpg",
+  "descripcion"
+);
 
-mates.push(mateImp)
-mates.push(mateTor)
-mates.push(mateCam)
-mates.push(bombAlp)
-mates.push(bombAce)
+mates.push(mateImp);
+mates.push(mateTor);
+mates.push(mateCam);
+mates.push(bombAlp);
 
+window.onload = () => {
+  let matesComprados = [];
+  let contenedor = document.getElementById(`cardContainer`);
 
-const calcularPrecioFinal = (lista) => {
-  let gastoTotal = 0;
-  lista.map((mate) => { gastoTotal += mate.precio })
-  return gastoTotal
-};   
+  mates.forEach((mate) => {
+    contenedor.innerHTML =
+      contenedor.innerHTML +
+      `
+      <div class="card mate" style="width: 18rem; margin-bottom: 10px;" >
+      <img src=${mate.imagen} class="card-img-top p-2 card1" alt="mate.imperial">
+      <div class="card-body">
+        <h5 class="card-title name">${mate.nombre}</h5>
+        <p class="card-text">${mate.descripcion}</p>
+        <p class="card-text price">$${mate.precio}</p>
+        <button class="btn btn-primary add-to-cart" data-id=${mate.id} data-price=${mate.precio}
+        data-name=${mate.nombre}>Agregar al carrito</button>
+      </div>`;
+  });
 
-const mostrarProductos = (lista) => {
-  let mensaje = `Su compra es: ` 
-  lista.map((mate) => {mensaje += ` | ` +  mate.nombre + `  ` + mate.precio + ` | `})
-   return mensaje
-}
+  const modal = document.querySelector(`.modal-body`);
+  const cartBtn = document.querySelector(".carrito");
 
-let continuar = confirm(`¿Desea comprar?`);
-let precioFinal = 0 
+  const matesArr = document.getElementsByClassName("mate");
+  for (let i = 0; i < matesArr.length; i++) {
+    matesArr[i].addEventListener("click", function (e) {
+      let mate = e.target.parentElement;
+      let mateComprado = {
+        nombre: mate.querySelector(".name").innerText,
+        precio: mate.querySelector(".price").innerText,
+      };
+      let matesNombres = [];
+      matesNombres.push(mateComprado.nombre);
 
-if (continuar) {
-  let matesComprados = []
-  while (continuar) {
-    let producto =
-      prompt(`Te presentamos nuestro catálogo. ¿Qué producto te interesa? Escribí su código!
-      ${mates[0].id} - ${mates[0].nombre} ${mates[0].precio}
-      ${mates[1].id} - ${mates[1].nombre} ${mates[1].precio}
-      ${mates[2].id} - ${mates[2].nombre} ${mates[2].precio}
-      ${mates[3].id} - ${mates[3].nombre} ${mates[3].precio}
-      ${mates[4].id} - ${mates[4].nombre} ${mates[4].precio}
-      AN6 - Salir
-      Gastaste: ${precioFinal}`);
-    
-    switch (producto) {
-      case "AN1":
-        matesComprados.push(mateImp);
-        break;
-      case "AN2":
-        matesComprados.push(mateTor);
-        break;
-      case "AN3":
-        matesComprados.push(mateCam)
-        break;
-      case "AN4":
-        matesComprados.push(bombAlp)
-        break;
-      case "AN5":
-        matesComprados.push(bombAce);
-        break;
-      case "AN6":
-        continuar = false;
-        break;
-      default:
-        alert("Ingresaste un valor incorrecto");
-        break;
-    }
-     precioFinal = calcularPrecioFinal(matesComprados)
+      matesComprados.push({
+        nombre: mate.querySelector(".name").innerText,
+        precio: mate.querySelector(".price").innerText,
+      });
+      console.log(matesNombres.indexOf(mateComprado.nombre));
 
-    if (producto != "AN6")
-      continuar = confirm(`Desea seguir comprando? 
-    - ACEPTAR para seguir
-    - CANCELAR para finalizar.`);
+      cartBtn.innerHTML = `Carrito ${matesComprados.length} `;
+      console.log(matesComprados);
+      console.log(matesComprados.indexOf(mateComprado.id));
+      modal.innerHTML =
+        modal.innerHTML +
+        `
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${mateComprado.nombre}</h5>
+              <p class="card-text"><small class="text-muted">${mateComprado.precio}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    });
   }
-
-  alert(`${mostrarProductos(matesComprados)} y el gasto total es de: $${precioFinal}`);
-
-let flag = true
-
-while (flag){
-  let pago = prompt(`¿Con qué método preferís abonar?: 
-    1 - Tarjeta de crédito (3 cuotas s/ interés disponibles).
-    2 - Tarjeta de débito / Transferencia bancaria.
-    3 - Efectivo (10% OFF).
-Ingresá el número de opción que elijas.`);
-
-  if (pago === "1") {
-    if (confirm (`¿Está seguro que desea pagar con tarjeta de crédito?`)){
-      alert(
-        `El gasto total es de $${precioFinal}, y tu pago lo realizarás en 3 cuotas de $${
-          (precioFinal / 3).toFixed(2)
-        } cada una.`
-      );
-      flag = false 
-    }
-    
-  } else if (pago === "2") {
-    if (confirm (`¿Está seguro que desea pagar con tarjeta de débito?`)){
-    alert(
-      `El gasto total es de $${precioFinal}. Nos estaremos comunicándo con vos para enviarte los datos bancarios para transferir el dinero.`
-    );
-    flag = false
-    }
-  } else if (pago === "3") {
-    if (confirm (`¿Está seguro que desea pagar con efectivo?`)){
-    alert(`El gasto total es de $${precioFinal * 0.9}.`);
-    flag = false
-    }
-  } else {
-    alert(`Error! Ingresá correctamente la opción.`);
-  }
-  }
-} 
-
-alert(`Te agradecemos por visitar nuestra web. La Vida es mejor con un Mate! Salí a conocer el mundo.`)
-
-
-
-
-
+};
