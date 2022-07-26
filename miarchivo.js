4ab0a6b3b1f1f658424bdcd4386b1cb0b6c89217
giuligelardi <ggelardi98@gmail.com>
2022-07-25 23:05:50 -0300

    segunda pre entrega

--- miarchivo.js
+++ miarchivo.js
@@ -58,6 +58,7 @@ window.onload = () => {
         <h5 class="card-title name">${mate.nombre}</h5>
         <p class="card-text">${mate.descripcion}</p>
         <p class="card-text price">$${mate.precio}</p>
+        <p class="d-none id">${mate.id}</p>
         <button class="btn btn-primary add-to-cart" data-id=${mate.id} data-price=${mate.precio}
         data-name=${mate.nombre}>Agregar al carrito</button>
       </div>`;
@@ -67,38 +68,76 @@ window.onload = () => {
   const cartBtn = document.querySelector(".carrito");
 
   const matesArr = document.getElementsByClassName("mate");
+  let matesIds = [];
+  let chartItems = 0;
   for (let i = 0; i < matesArr.length; i++) {
     matesArr[i].addEventListener("click", function (e) {
       let mate = e.target.parentElement;
       let mateComprado = {
         nombre: mate.querySelector(".name").innerText,
+        id: mate.querySelector(".id").innerText,
         precio: mate.querySelector(".price").innerText,
+        cantidad: 1
       };
-      let matesNombres = [];
-      matesNombres.push(mateComprado.nombre);
-
-      matesComprados.push({
-        nombre: mate.querySelector(".name").innerText,
-        precio: mate.querySelector(".price").innerText,
-      });
-      console.log(matesNombres.indexOf(mateComprado.nombre));
-
-      cartBtn.innerHTML = `Carrito ${matesComprados.length} `;
-      console.log(matesComprados);
-      console.log(matesComprados.indexOf(mateComprado.id));
-      modal.innerHTML =
-        modal.innerHTML +
-        `
-        <div class="card mb-3" style="max-width: 540px;">
+      if (matesIds.indexOf(mateComprado.id) === -1) {
+        matesIds.push(mateComprado.id);
+        chartItems += 1;
+        cartBtn.innerHTML = `Carrito ${chartItems} `;
+        modal.innerHTML =
+          modal.innerHTML +
+          `
+        <div id=${mateComprado.id} class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">${mateComprado.nombre}</h5>
               <p class="card-text"><small class="text-muted">${mateComprado.precio}</small></p>
             </div>
+            <p class="card-text quantity">${mateComprado.cantidad}</p>
           </div>
         </div>
       </div>`;
+      } else {
+        const x = document.getElementById(mateComprado.id)
+        let quantum = parseInt(x.querySelector('.quantity').innerText)
+        x.querySelector('.quantity').innerText = quantum + 1
+      }
     });
   }
 };
+
+const contacto = document.getElementById(`contacto`);
+const alert = document.getElementById(`alert`);
+const bienvenida = document.getElementById(`bienvenida`);
+
+contacto.onsubmit = (e) => {
+  e.preventDefault();
+  let info = {};
+  if (e.target.children.length > 0) {
+    for (const el of e.target.children) {
+      const input = el.querySelector("input");
+      if (input && input.value && input.type !== `submit`) {
+        info[input.id] = input.value;
+        const obj = {};
+        obj[`nombre`] = input.id;
+        obj[`valor`] = input.value;
+        localStorage.setItem(obj.nombre, obj.valor);
+      }
+    }
+  }
+  if (info.Nombre) {
+    alert.innerText = `Gracias ${info.Nombre} ${info.Apellido} por registrarte. Te estaremos enviando tu código de descuento vía drone.`;
+    alert.classList.remove(`d-none`);
+    setTimeout(() => {
+      alert.classList.add(`d-none`);
+    }, 5000);
+    contacto.reset();
+  }
+};
+
+const nombre = localStorage.getItem(`Nombre`);
+const apellido = localStorage.getItem(`Apellido`);
+
+if (nombre || apellido) {
+  bienvenida.innerText = `Bienvenido ${nombre} ${apellido}!`;
+}
